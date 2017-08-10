60efee2c4d296b1c27bc6734e95425248d23f3db
Steven Klass <rh0dium@users.noreply.github.com>
2017-08-10 11:42:31 -0700

    Initial cut

---
+++ django_datatable_view_user_columns/urls.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""urls.py: Django """
+
+from __future__ import unicode_literals
+from __future__ import print_function
+
+import logging
+
+__author__ = 'Steven Klass'
+__date__ = '8/10/17 11:41'
+__copyright__ = 'Copyright 2017 IC Manage. All rights reserved.'
+__credits__ = ['Steven Klass', ]
+
+log = logging.getLogger(__name__)
---
+++ setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='customer_x',
+    version='0.1.0',
+    description='Django Datatable view user columns',
+    long_description=readme,
+    author='Steven Klasss',
+    author_email='sklass@icmanage.com',
+    url='https://github.com/icmanage/django-datatable-view-user-columns',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs')),
+)
+
