b0625dc9b80116f0b29f5df4fb9005b5651669a6
Steven Klass <rh0dium@users.noreply.github.com>
2018-04-26 11:22:31 -0600

    Handle Django 1.11

--- setup.py
+++ setup.py
@@ -137,7 +137,7 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['django>=1.7,<1.9', 'django-datatable-view'],  # Optional
+    install_requires=['django>=1.7,<2.0', 'django-datatable-view'],  # Optional
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
